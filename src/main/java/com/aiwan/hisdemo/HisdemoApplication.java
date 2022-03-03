86ee486f8373b74ef5a5549a99eaa09ee6bd5771
zhangwenlong <wenlongzhang@linkdoc.com>
2022-03-03 18:37:08 +0800

    test

---
+++ src/main/java/com/aiwan/hisdemo/HisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.aiwan.hisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aiwan/hisdemo/HisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiwan.hisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
